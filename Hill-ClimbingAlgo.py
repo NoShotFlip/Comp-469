b9f5df7ad2be7508baa6689f1c81f345a6756bda
NoShotFlip <139315237+NoShotFlip@users.noreply.github.com>
2024-08-09 10:34:31 -0700

    Create Hill-ClimbingAlgo.py
    Not Sure if its how he wants it but the algo is there

---
+++ Hill-ClimbingAlgo.py
@@ -0,0 +1,75 @@
+import random
+
+def print_board(board):
+    for row in board:
+        print(" ".join(str(cell) for cell in row))
+    print()
+
+def create_board():
+    return [[0]*8 for _ in range(8)]
+
+def place_queens(board):
+    for i in range(8):
+        row = random.randint(0, 7)
+        board[row][i] = 1
+    return board
+
+def get_attacking_pairs(board):
+    attacking_pairs = 0
+    for i in range(8):
+        for j in range(8):
+            if board[i][j] == 1:
+                for k in range(8):
+                    if k != j and board[i][k] == 1:
+                        attacking_pairs += 1
+                    if k != i and board[k][j] == 1:
+                        attacking_pairs += 1
+                for k in range(1, 8):
+                    if i+k < 8 and j+k < 8 and board[i+k][j+k] == 1:
+                        attacking_pairs += 1
+                    if i-k >= 0 and j+k < 8 and board[i-k][j+k] == 1:
+                        attacking_pairs += 1
+                    if i+k < 8 and j-k >= 0 and board[i+k][j-k] == 1:
+                        attacking_pairs += 1
+                    if i-k >= 0 and j-k >= 0 and board[i-k][j-k] == 1:
+                        attacking_pairs += 1
+    return attacking_pairs // 2
+
+def get_neighbors(board):
+    neighbors = []
+    for col in range(8):
+        for row in range(8):
+            if board[row][col] == 0:
+                new_board = [r[:] for r in board]
+                for r in range(8):
+                    new_board[r][col] = 0
+                new_board[row][col] = 1
+                neighbors.append(new_board)
+    return neighbors
+
+def hill_climb(board):
+    current_board = board
+    current_attacks = get_attacking_pairs(current_board)
+    while True:
+        neighbors = get_neighbors(current_board)
+        next_board = None
+        next_attacks = current_attacks
+        for neighbor in neighbors:
+            attacks = get_attacking_pairs(neighbor)
+            if attacks < next_attacks:
+                next_board = neighbor
+                next_attacks = attacks
+        if next_attacks >= current_attacks:
+            break
+        current_board = next_board
+        current_attacks = next_attacks
+    return current_board, current_attacks
+
+initial_board = place_queens(create_board())
+solution, attacks = hill_climb(initial_board)
+
+print("Initial Board:")
+print_board(initial_board)
+print("Solution Board:")
+print_board(solution)
+print(f"Attacking Pairs: {attacks}")
